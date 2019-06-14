1c8b7043c94aa42f0cd41bd78824cf733a29a1a6
paul-altyfc <paul.m.dewey@gmail.com>
2019-06-14 16:35:28 +0100

    completed redoMenu

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ exports.redoMenu = (menu, chef, dietary_req) => {
   return menu.reduce((newMenu, currMenu) => {
     currMenu.recommended_course = currMenu.course;
     delete currMenu.course;
+    // currMenu.id = parseInt(currMenu.id, 10);
+    currMenu.id = +currMenu.id;
     if (chef) currMenu.chef = chef;
+    if (dietary_req) currMenu[dietary_req] = true;
     newMenu.push(currMenu);
 
     return newMenu;
--- index.spec.js
+++ index.spec.js
@@ -22,7 +22,7 @@ describe('redoMenu', () => {
     const actual = redoMenu(menu);
     const expected = [
       {
-        id: '2',
+        id: 2,
         recommended_course: 'main',
         dish: 'dahl',
         special: true
@@ -30,7 +30,7 @@ describe('redoMenu', () => {
     ];
     expect(actual).to.eql(expected);
   });
-  it('returns a menu array with the chef added', () => {
+  it('returns a menu array with the chef added and id changed to a number', () => {
     //
     const menu = [
       {
@@ -44,11 +44,66 @@ describe('redoMenu', () => {
     const actual = redoMenu(menu, chef);
     const expected = [
       {
-        id: '2',
+        id: 2,
         recommended_course: 'main',
         dish: 'dahl',
         special: true,
         chef: 'jonny'
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
+  it('returns a menu array with the dietary requirement added as a key', () => {
+    //
+    const menu = [
+      {
+        id: '2',
+        course: 'main',
+        dish: 'dahl',
+        special: true
+      }
+    ];
+    const chef = 'jonny';
+    const diet_req = 'gluten free';
+    const actual = redoMenu(menu, chef, diet_req);
+    const expected = [
+      {
+        id: 2,
+        recommended_course: 'main',
+        dish: 'dahl',
+        special: true,
+        chef: 'jonny',
+        'gluten free': true
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
+  it('returns a new menu array when passed multiple menus', () => {
+    //
+    const menu = [
+      { id: '1', course: 'starter', dish: 'salad' },
+      { id: '2', course: 'main', dish: 'spaghetti', special: true }
+    ];
+
+    const chef = 'izzy';
+
+    const diet_req = 'nut free';
+    const actual = redoMenu(menu, chef, diet_req);
+    const expected = [
+      {
+        id: 1,
+        recommended_course: 'starter',
+        dish: 'salad',
+        chef: 'izzy',
+        'nut free': true
+      },
+      {
+        id: 2,
+        recommended_course: 'main',
+        dish: 'spaghetti',
+        special: true,
+        chef: 'izzy',
+        'nut free': true
       }
     ];
     expect(actual).to.eql(expected);
